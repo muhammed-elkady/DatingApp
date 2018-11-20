bcdbe4bad5549b234cd327d4d0cbe4a2e3bd587a
Muhammed Elkady <Muhammedelkady@hotmail.com>
2018-11-20 09:33:21 +0200

    added nav component

--- DatingApp.Spa/ClientApp/src/app/app.module.ts
+++ DatingApp.Spa/ClientApp/src/app/app.module.ts
@@ -3,23 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { NavComponent } from '../app/nav/nav.component';
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
 
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent
+
+   ],
+   imports: [
+      BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+      HttpClientModule,
+      FormsModule,
+      RouterModule.forRoot([
+
+      ])
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
