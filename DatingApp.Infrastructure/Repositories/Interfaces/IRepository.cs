8ebb4617db63f790eacb43134529636e2d3e004c
Muhammed Elkady <Muhammedelkady@hotmail.com>
2018-12-01 13:32:05 +0200

    added: user repository

---
+++ DatingApp.Infrastructure/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Infrastructure.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        Task<bool> SaveAll();
+    }
+}
--- DatingApp.Infrastructure/Repositories/UserRepository.cs
+++ DatingApp.Infrastructure/Repositories/UserRepository.cs
@@ -1,11 +1,12 @@
-﻿using DatingApp.Core.Identity;
-using DatingApp.Infrastructure.Repositories.Interfaces;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using DatingApp.Core.Identity;
+using DatingApp.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Infrastructure.Repositories
 {
@@ -18,12 +19,32 @@ namespace DatingApp.Infrastructure.Repositories
             _context = context;
         }
 
+        public void Add(ApplicationUser entity)
+        {
+
+            _context.Add(entity);
+        }
+
+        public void Delete(ApplicationUser entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
         public IEnumerable<ApplicationUser> GetUsersWithTheirRoles()
         {
             var myUsersAndRoles = _context.Users.Include(u => u.UserRoles).ToList();
             return myUsersAndRoles;
         }
-
 
     }
 }
