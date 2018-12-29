3cd708e674a9cc3542899e61fd62e1ed17aa4da5
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2018-12-29 19:18:58 +0200

    adding npm package: ng file uploader

--- DatingApp.Spa/ClientApp/src/app/app.module.ts
+++ DatingApp.Spa/ClientApp/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
    declarations: [
@@ -49,6 +50,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
       FormsModule,
       ReactiveFormsModule,
       NgxGalleryModule,
+      FileUploadModule,
       RouterModule.forRoot(appRoutes),
       BsDropdownModule.forRoot(),
       TabsModule.forRoot()
--- DatingApp.Spa/ClientApp/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.Spa/ClientApp/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,9 @@
+import { AuthService } from './../../_services/auth.service';
+import { environment } from './../../../environments/environment';
+
 import { Photo } from './../../models/interfaces/photo';
 import { Component, OnInit, Input } from '@angular/core';
-
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,9 +13,32 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PhotoEditorComponent implements OnInit {
 
   @Input() photos: Photo[];
-  constructor() { }
+  private baseUrl = environment.apiUrl;
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
+
   }
 
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: `${this.baseUrl}/users/${this.authService.decodedToken.nameid}/photos`,
+      authToken: `Bearer ${this.authService.token}`,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 10 * 1024
+    });
+    
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+  }
 }
