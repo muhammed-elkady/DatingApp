108479fc467aefc79ac99141fd51aeee055140d4
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2019-01-05 16:12:57 +0200

    added: additional fields to UserForRegisterDTO

--- DatingApp.Core/Dtos/User/UserForRegisterDto.cs
+++ DatingApp.Core/Dtos/User/UserForRegisterDto.cs
@@ -9,8 +9,8 @@ namespace DatingApp.Core.Dtos.User
     {
         public UserForRegisterDto()
         {
-            //Created = DateTime.Now;
-            //LastActive = DateTime.Now;
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
         }
         [Required]
         public string Username { get; set; }
@@ -19,22 +19,24 @@ namespace DatingApp.Core.Dtos.User
         [StringLength(20, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 20 characters")]
         public string Password { get; set; }
 
-        //[Required]
-        //public string Gender { get; set; }
+        [Required]
+        public string Gender { get; set; }
 
-        //[Required]
-        //public string KnownAs { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
 
-        //[Required]
-        //public DateTime DateOfBirth { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
 
-        //[Required]
-        //public string City { get; set; }
+        [Required]
+        public string City { get; set; }
 
-        //[Required]
-        //public string Country { get; set; }
-        //public DateTime Created { get; set; }
-        //public DateTime LastActive { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
 
 
     }
