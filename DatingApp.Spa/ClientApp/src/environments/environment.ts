24b9219b691e3fe674082882222e250965cc0051
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2019-01-18 18:17:01 +0200

    changed: the running port

--- DatingApp.Spa/ClientApp/src/environments/environment.ts
+++ DatingApp.Spa/ClientApp/src/environments/environment.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://localhost:44344/api/' ,
-  whitelistedDomains: ['https://localhost:44344/'],
-  blacklistedRoutes: ['https://localhost:44344/api/auth/']
+  apiUrl:'https://localhost:44304/api/' ,
+  whitelistedDomains: ['https://localhost:44304/'],
+  blacklistedRoutes: ['https://localhost:44304/api/auth/']
 
 
 };
