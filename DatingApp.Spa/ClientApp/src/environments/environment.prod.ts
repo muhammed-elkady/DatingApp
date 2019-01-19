5db0d779951fdce9d0ba851a1b9e4fe08422e00f
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2019-01-19 15:36:41 +0200

    upgrade to angular 7.2

--- DatingApp.Spa/ClientApp/src/environments/environment.prod.ts
+++ DatingApp.Spa/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://localhost:44304/api/",
+  whitelistedDomains: ["https://localhost:44304/"],
+  blacklistedRoutes: ["https://localhost:44304/api/auth/"]
 };
