3540d5035e96c45c0d764b5df7d855aab54e8888
Muhammed Mamdouh ElKady <MuhammedElKady@hotmail.com>
2019-01-24 17:00:55 +0200

    added: PaginationHeader + AddPaginationHeader method

---
+++ DatingApp.Core/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.Core.Helpers;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingApp.Core.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
